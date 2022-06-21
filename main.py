b0b51b9327aa4ef2bbca0ddaaff110a4c91f9be3
Shantnu Tiwari <shantnu@Shantnus-MacBook-Air.local>
2022-06-21 15:41:16 +0100

    adding health check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from get_senti.finance import get_finance_data
 from get_senti.get_reddit import get_redit_data
 from dynamodb.update_dynamo import update_dynamodb
+import requests
 
 import datetime
 
@@ -11,3 +12,4 @@ print("\n --------------")
 print(final_pos, final_neg, stock_percent)
 print("\n -------------- Updating dynamo \n")
 update_dynamodb(date_obj=datetime.datetime.now(), final_pos=final_pos, final_neg=final_neg, stock_percent=stock_percent)
+requests.get("https://hc-ping.com/f1c6213b-14fb-46fd-87f4-a83a7dbf2980")
